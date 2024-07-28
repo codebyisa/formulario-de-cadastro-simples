c7fd0af20a1b2ecef4d7f30776ca10a33ca25ff1
codebyisa <zadora011@gmail.com>
2024-07-27 22:13:15 -0300

    primeiro commit

--- scripts.js
+++ scripts.js
@@ -7,3 +7,12 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
     //Redireciona para a página de sucesso
     window.location.href = 'sucess.html';
 });
+
+  $(document).ready(function() {
+    $('#dataNascimento').datepicker({
+      dateFormat: 'dd/mm/yy', // Formato da data
+      changeYear: true,       // Permite selecionar o ano
+      yearRange: '1900:2024'  // Define o intervalo de anos
+      // Outras opções podem ser adicionadas conforme necessário
+    });
+  });
